91ff432a0347f783175e8b46d0d6f0157a09a77d
PixelSaver <stephen.k@proton.me>
2026-03-17 18:52:43 -0700

    Auth token works!!

--- src/auth.rs
+++ src/auth.rs
@@ -1,9 +1,20 @@
 use anyhow::Result;
 use pkce;
+use rand::{self, RngExt, distr::Distribution};
 use std::process::Command;
 use tiny_http::{Response, Server};
 
 const CLIENT_ID: &str = "56Q7QczaBEH6o8J9YTtMReNIsHOwOrZPB59RAZj64nI";
+const REDIRECT_URI: &str = "http://127.0.0.1:8080/callback";
+
+#[derive(serde::Deserialize)]
+struct TokenResponse {
+    access_token: String,
+    token_type: String,
+    expires_in: u64,
+    scope: String,
+    created_at: u64,
+}
 
 fn gen_pkce() -> (Vec<u8>, String) {
     let verify = pkce::code_verifier(128);
@@ -11,18 +22,33 @@ fn gen_pkce() -> (Vec<u8>, String) {
     (verify, challenge)
 }
 
-fn get_url(challenge: &str) -> Result<String> {
+fn get_auth_code_url(challenge: &str, state: &str) -> Result<String> {
     let client = reqwest::blocking::Client::new();
     let request = client
         .get("https://hackatime.hackclub.com/oauth/authorize")
         .query(&[
             ("client_id", CLIENT_ID),
-            ("redirect_uri", "http://127.0.0.1:8080/callback"),
+            ("redirect_uri", REDIRECT_URI),
             ("response_type", "code"),
             ("scope", "profile read"),
-            ("state", "RANDOM_STRING"),
+            ("state", &state),
             ("code_challenge", &challenge),
             ("code_challenge_method", "S256"),
+        ])
+        .build()?;
+    Ok(request.url().to_string())
+}
+
+fn get_token_url(verify: &str, auth_code: &str) -> Result<String> {
+    let client = reqwest::blocking::Client::new();
+    let request = client
+        .get("https://hackatime.hackclub.com/oauth/token")
+        .query(&[
+            ("client_id", CLIENT_ID),
+            ("code", &auth_code),
+            ("redirect_uri", REDIRECT_URI),
+            ("grant_type", "authorization_code"),
+            ("code_verifier", &verify),
         ])
         .build()?;
     Ok(request.url().to_string())
@@ -67,14 +93,29 @@ fn open_browser(auth_url: &str) {
 }
 
 pub fn auth_user() -> Result<()> {
-    let (_verifier, challenge) = gen_pkce();
+    let (verifier, challenge) = gen_pkce();
+    let state: String = rand::rng()
+        .sample_iter(rand::distr::Alphanumeric)
+        .take(16)
+        .map(char::from)
+        .collect();
 
-    let auth_url = get_url(&challenge)?;
+    let auth_url = get_auth_code_url(&challenge, &state)?;
 
     open_browser(&auth_url);
     let code = wait_for_code()?;
 
     println!("Got code: {}", code);
+    
+    let token_url = get_token_url(&String::from_utf8(verifier)?, &code)?;
+    
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .post(&token_url)
+        .send()?
+        .json::<TokenResponse>()?;
+    
+    println!("Auth code: {}", response.access_token);
 
     Ok(())
 }
